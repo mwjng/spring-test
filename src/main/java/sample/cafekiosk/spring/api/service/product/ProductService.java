c17f114e7ead45afc0ecce7561ce152545003397
wmw04087 <wmw04087@gmail.com>
2024-10-24 15:46:28 +0900

    presentation layer (1)

--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -2,6 +2,8 @@ package sample.cafekiosk.spring.api.service.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
@@ -9,11 +11,31 @@ import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 
 import java.util.List;
 
+/**
+ * Transactional (Read Only)
+ * CQRS - Command / Query
+ * DB Replication
+ * Master - Command
+ * Slave - Read
+ */
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
+
+    // 동시성 이슈
+    // unique index, UUID, ...
+    @Transactional
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        String nextProductNumber = createNextProductNumber();
+
+        Product product = request.toEntity(nextProductNumber);
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.of(savedProduct);
+    }
 
     public List<ProductResponse> getSellingProducts() {
         List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
@@ -22,5 +44,16 @@ public class ProductService {
                 .map(ProductResponse::of)
                 .toList();
     }
-}
 
+    private String createNextProductNumber() {
+        String latestProductNumber = productRepository.findLatestProductNumber();
+        if(latestProductNumber == null) {
+            return "001";
+        }
+
+        int lastestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = lastestProductNumberInt + 1;
+
+        return String.format("%03d", nextProductNumberInt);
+    }
+}
