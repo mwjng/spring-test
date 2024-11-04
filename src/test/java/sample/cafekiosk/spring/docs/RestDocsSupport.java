748958eb16727b3cebb848c36f4b8bbad213fba3
wmw04087 <wmw04087@gmail.com>
2024-11-04 17:21:34 +0900

    spring rest docs

---
+++ src/test/java/sample/cafekiosk/spring/docs/RestDocsSupport.java
@@ -0,0 +1,37 @@
+package sample.cafekiosk.spring.docs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
+@ExtendWith(RestDocumentationExtension.class)
+//@SpringBootTest
+public abstract class RestDocsSupport {
+
+    protected MockMvc mockMvc;
+    protected ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    void setUp(RestDocumentationContextProvider provider) {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(initController())
+            .apply(documentationConfiguration(provider))
+            .build();
+    }
+
+    protected abstract Object initController();
+
+
+//    @BeforeEach
+//    void setUp(WebApplicationContext webApplicationContext,
+//               RestDocumentationContextProvider provider) {
+//        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+//            .apply(documentationConfiguration(provider))
+//            .build();
+//    }
+}
